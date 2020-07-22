356226639f504fe704acbcc0911d18df83b0ec5e
Crefax <48588995+Crefax@users.noreply.github.com>
2020-07-22 20:39:49 +0000

    Update crefax.py

--- crefax.py
+++ crefax.py
@@ -8,207 +8,218 @@ var = 1
 while var == 1 :
 
 	print("""
-\033[92m1) Port Tarama
-2) Zaafiyet
-3) VPN Kontrol
-4) WordPress Tarama
-5) Mac değiştirme
-6) Güvenlik Duvarı tespit
-7) Port KabaKuvvet Saldırısı
-8) WebSitesi uzantı tespit
-9) Python derleyici
+\033[92m
+1) Port İşlemleri
+2) Web Saldırıları
+3) Diğer
 
 0) Setup & Update
 00) Çıkış
 	""")
-	anaislem = raw_input("İşlem no seçin: ")
+	kategori = raw_input("İşlem no seçin: ")
+	if(kategori == "1"): #Port İşlemleri başlangıç
+		os.system("figlet Port İşlemleri")
+		print("""
+1) Port Tarama
+2) Port Kaba Kuvvet Saldırısı
 
-	if(anaislem == "1"):
-		os.system("clear")
-		os.system("figlet Port Tarama")
+		""")
+		islem = raw_input("İşlem no seçin: ")
+		if(islem == "1"):
+			os.system("clear")
+			os.system("figlet Port Tarama")
 
-		print ("""
+			print ("""
 1) Hızlı tarama
 2) Servis ve Versiyon Bilgisi
 3) İşletim Sistemi Bilgisi
 
 Bu Program nmap aracını kullanarak port tarama vb işlemleri yapar.
-		""")
+			""")
 
-		islemno = raw_input("İşlem girin: ")
+			islemno = raw_input("İşlem girin: ")
 
-		if(islemno == "1"):
-			hedefip = raw_input("Hedef ip girin: ")
-			os.system("nmap " + hedefip)
+			if(islemno == "1"):
+				hedefip = raw_input("Hedef ip girin: ")
+				os.system("nmap " + hedefip)
 
-		elif(islemno == "2"):
-			hedefip = raw_input("Hedef ip girin: ")
-			os.system("nmap -sS -sV " + hedefip)
+			elif(islemno == "2"):
+				hedefip = raw_input("Hedef ip girin: ")
+				os.system("nmap -sS -sV " + hedefip)
 
-		elif(islemno == "3"):
-			hedefip = raw_input("Hedef ip girin: ")
-			os.system("nmap -O " + hedefip)
+			elif(islemno == "3"):
+				hedefip = raw_input("Hedef ip girin: ")
+				os.system("nmap -O " + hedefip)
+		elif(islem == "2"):
+			os.system("clear")
+			os.system("figlet Port Kaba Kuvvet")
+			print("""
+1) FTP
+2) SSH
+3) Telnet
+4) HTTP
+5) SMB
+6) RDP
+7) MySQL
+			""")
 
-	elif(anaislem == "2"):
-		os.system("apt-get install figlet")
+			islemno = raw_input("İşlem No Girin: ")
+			hedefip = raw_input("Hedef İp Girin: ")
+			kullaniciadi = raw_input("Kullanıcı Adı Dosya Yolu: ")
+			sifre = raw_input("Sifrelerin Bulunduğu Dosya Yolu: ")
+
+			if(islemno == "1"):
+				os.system("ncrack -p 21 -U " + kullaniciadi + " -P " + sifre + " " + hedefip)
+
+			if(islemno == "2"):
+				os.system("ncrack -p 22 -U " + kullaniciadi + " -P " + sifre + " " + hedefip)
+
+			if(islemno == "3"):
+				os.system("ncrack -p 23 -U " + kullaniciadi + " -P " + sifre + " " + hedefip)
+			if(islemno == "4"):
+				os.system("ncrack -p 80 -U " + kullaniciadi + " -P " + sifre + " " + hedefip)
+			if(islemno == "5"):
+				os.system("ncrack -p 139 -U " + kullaniciadi + " -P " + sifre + " " + hedefip)
+			if(islemno == "6"):
+				os.system("ncrack -p 3389 -U " + kullaniciadi + " -P " + sifre + " " + hedefip)
+			if(islemno == "7"):
+				os.system("ncrack -p 3306 -U " + kullaniciadi + " -P " + sifre + " " + hedefip)
+
+	elif(kategori == "2"): #Web saldırıları başlangıç
 		os.system("clear")
-		os.system("figlet Zaafiyet Tespit")
+		os.system("figlet Web Attack tools")
 		print("""
+1) WebSitesi Zaafiyet Taraması
+2) WebSitesi uzantı tespit
+3) WebSitesi GÜvenlik Duvarı Tespit
+4) VPN Kontrol
+5) Wordpress Taraması
+		""")		
+		islem = raw_input("İşlem no girin: ")
+		if(islem == "1"):
+			os.system("clear")
+			os.system("figlet Zaafiyet Tespit")
+			print("""
 Bu Programda nikto aracı ile zaafiyet tespiti yapılıyor.
-		""")
+			""")
 
-		hedefip = raw_input("Hedef ip girin: ")
-		os.system("nikto -h " + hedefip)
+			hedefip = raw_input("Hedef ip girin: ")
+			os.system("nikto -h " + hedefip)
+		elif(islem == "2"):
+			os.system("clear")
+			os.system("figlet Dirb")
+			dirburl = raw_input("Taranacak web sitesinin urlsi: ")
+			os.system("dirb " + dirburl)
+		elif(islem == "3"):
+			os.system("clear")
+			os.system("figlet Guvenlik Duvari Tespit")
+			site = raw_input("Site Adresini Girin: ")
+			os.system("wafw00f " + site)
+		elif(islem == "4"):
+			os.system("clear")
+			os.system("figlet VPN Kontrol")
+			print("""
+	VPN Kontrol Programına Hoş Geldiniz.
+			""")
 
+			hedefip = raw_input("Hedef İp Girin: ")
 
-	elif(anaislem == "3"):
-		os.system("clear")
-		os.system("figlet VPN Kontrol")
-		print("""
-VPN Kontrol Programına Hoş Geldiniz.
-		""")
-
-		hedefip = raw_input("Hedef İp Girin: ")
-
-		os.system("ike-scan " + hedefip)
-	elif(anaislem == "4"):
-		os.system("clear")
-		os.system("figlet WORDPRESS TARAMA")
-		print("""
+			os.system("ike-scan " + hedefip)
+		elif(islem == "5"):
+			os.system("clear")
+			os.system("figlet WORDPRESS TARAMA")
+			print("""
 Wordpress Tarama Programına Hoş Geldiniz. 
 
 1) Hızlı Tarama
 2) Eklenti Tarama
 3) Tema Tarama
 4) Yönetici Kullanıcı Adı Tarama
-		""")
+			""")
 
-		islemno = raw_input("İşlem Numarasını Girin: ")
+			islemno = raw_input("İşlem Numarasını Girin: ")
 
-		if(islemno=="1"):
-			site = raw_input("Site Adresi Girin: ")
-			os.system("wpscan --url " + site)
+			if(islemno=="1"):
+				site = raw_input("Site Adresi Girin: ")
+				os.system("wpscan --url " + site)
 
-		elif(islemno == "2"):
-			site = raw_input("Site Adresi Girin: ")
-			os.system("wpscan --url " + site + " --enumerate p")
+			elif(islemno == "2"):
+				site = raw_input("Site Adresi Girin: ")
+				os.system("wpscan --url " + site + " --enumerate p")
 
-		elif(islemno == "3"):
-			site = raw_input("Site Adresi Girin: ")
-			os.system("wpscan --url " + site + " --enumerate t")
+			elif(islemno == "3"):
+				site = raw_input("Site Adresi Girin: ")
+				os.system("wpscan --url " + site + " --enumerate t")
 
-		elif(islemno == "4"):
-			site = raw_input("Site Adresi Girin: ")
-			os.system("wpscan --url " + site + " --enumerate u")
+			elif(islemno == "4"):
+				site = raw_input("Site Adresi Girin: ")
+				os.system("wpscan --url " + site + " --enumerate u")
 
-		else:
-			print("Hatalı Seçim Yaptınız. Program Kapatılıyor.")
 
-	elif(anaislem == "5"):
+	elif(kategori == "3"):
 		os.system("clear")
-		os.system("figlet MAC Degistirme")
+		os.system("figlet Diğer")
+
 		print("""
+1) Mac Adresi değiştirme
+2) Python derleyici
+		""")
+		islem = raw_input("İşlem no girin: ")
+
+		if(islem == "1"):
+			os.system("clear")
+			os.system("figlet MAC Degistirme")
+			print("""
 MAC Adres Değiştirme Programına Hoş Geldiniz. 
 
 1) MAC Adresi Random Belirle
 2) MAC Adresi Elle Belirle
 3) MAC Adresi Orijinale Döndür
-		""")
+			""")
 
-		islemno = raw_input("İşlem No Girin: ")
+			islemno = raw_input("İşlem No Girin: ")
 
-		if(islemno=="1"):
-			os.system("ifconfig eth0 down")
-			os.system("macchanger -r eth0")
-			os.system("ifconfig eth0 up")
-			print("\033[92mYeni MAC Adresi Random Belirlendi.")
-
-
-		if(islemno=="2"):
-			macadres = raw_input("Yeni MAC Adres Girin: ")
-			os.system("ifconfig eth0 down")
-			os.system("macchanger --mac " + macadres + " eth0")
-			os.system("ifconfig eth0 up")
-			print("\033[92mYeni MAC Adresi Elle Belirlendi.")
-
-		if(islemno=="3"):
-			os.system("ifconfig eth0 down")
-			os.system("macchanger -p eth0")
-			os.system("ifconfig eth0 up")
-			print("\033[92mMAC Adresi Orijinale Döndürüldü.")
-
-	elif(anaislem == "6"):
-		os.system("clear")
-		os.system("figlet Guvenlik Duvari Tespit")
-		print("""
-
-		""")
-
-		site = raw_input("Site Adresini Girin: ")
-		os.system("wafw00f " + site)
-	elif(anaislem == "7"):
-		os.system("clear")
-		os.system("figlet Port Kaba Kuvvet")
-		print("""
-		1) FTP
-		2) SSH
-		3) Telnet
-		4) HTTP
-		5) SMB
-		6) RDP
-		7) MySQL
-		""")
-
-		islemno = raw_input("İşlem No Girin: ")
-		hedefip = raw_input("Hedef İp Girin: ")
-		kullaniciadi = raw_input("Kullanıcı Adı Dosya Yolu: ")
-		sifre = raw_input("Sifrelerin Bulunduğu Dosya Yolu: ")
-
-		if(islemno == "1"):
-			os.system("ncrack -p 21 -U " + kullaniciadi + " -P " + sifre + " " + hedefip)
-
-		if(islemno == "2"):
-			os.system("ncrack -p 22 -U " + kullaniciadi + " -P " + sifre + " " + hedefip)
-
-		if(islemno == "3"):
-			os.system("ncrack -p 23 -U " + kullaniciadi + " -P " + sifre + " " + hedefip)
-		if(islemno == "4"):
-			os.system("ncrack -p 80 -U " + kullaniciadi + " -P " + sifre + " " + hedefip)
-		if(islemno == "5"):
-			os.system("ncrack -p 139 -U " + kullaniciadi + " -P " + sifre + " " + hedefip)
-		if(islemno == "6"):
-			os.system("ncrack -p 3389 -U " + kullaniciadi + " -P " + sifre + " " + hedefip)
-		if(islemno == "7"):
-			os.system("ncrack -p 3306 -U " + kullaniciadi + " -P " + sifre + " " + hedefip)
-
-	elif(anaislem == "8"):
-		os.system("clear")
-		os.system("figlet Dirb")
-		dirburl = raw_input("Taranacak web sitesinin urlsi: ")
-		os.system("dirb " + dirburl)
+			if(islemno=="1"):
+				os.system("ifconfig eth0 down")
+				os.system("macchanger -r eth0")
+				os.system("ifconfig eth0 up")
+				print("\033[92mYeni MAC Adresi Random Belirlendi.")
 
 
-	elif(anaislem == "9"):
-		os.system("clear")
-		os.system("figlet Derleyici")
+			if(islemno=="2"):
+				macadres = raw_input("Yeni MAC Adres Girin: ")
+				os.system("ifconfig eth0 down")
+				os.system("macchanger --mac " + macadres + " eth0")
+				os.system("ifconfig eth0 up")
+				print("\033[92mYeni MAC Adresi Elle Belirlendi.")
 
-		print("""
-Python dosyalarınızı derleyerek kodlarını okunmaz hale getirin py uznatısını pyc olarak değiştirir.
-		""")
+			if(islemno=="3"):
+				os.system("ifconfig eth0 down")
+				os.system("macchanger -p eth0")
+				os.system("ifconfig eth0 up")
+				print("\033[92mMAC Adresi Orijinale Döndürüldü.")
 
-		derle = raw_input("Dosyanızın İsmini Girin: ")
+		elif(islem == "2"):
+			os.system("clear")
+			os.system("figlet Derleyici")
 
-		py_compile.compile(derle)
+			print("""
+	Python dosyalarınızı derleyerek kodlarını okunmaz hale getirin py uznatısını pyc olarak değiştirir.
+			""")
+
+			derle = raw_input("Dosyanızın İsmini Girin: ")
+
+			py_compile.compile(derle)
 
 
-	elif(anaislem == "0"):
+	elif(kategori == "0"):
 		os.system("apt-get install figlet")
 		os.system("apt-get install nikto")
 		os.system("apt-get install wpscan")
 		os.system("apt-get install nmap")
 		os.system("apt-get install wafw00f")
 		os.system("git clone https://github.com/Crefax/tr-tools.git")
-		os.system("cp tr-tools/crefax.py .")
+		os.system("cp tr-tools/tr-tools.py .")
 		os.system("rm -r tr-tools")
-	elif(anaislem == "00"):
+	elif(kategorikategori == "00"):
 		break
